148d4727f3b58e469048d553d73252590bce5c16
AlexWalker28 <alexwolk28@gmail.com>
2017-06-10 14:42:35 +0600

    Add CustomAddAuthorAdapter

--- app/src/main/java/com/projects/thirtyseven/glue/AddAuthorActivity.java
+++ app/src/main/java/com/projects/thirtyseven/glue/AddAuthorActivity.java
@@ -2,6 +2,7 @@ package com.projects.thirtyseven.glue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class AddAuthorActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class AddAuthorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_author);
+
     }
 }
---
+++ app/src/main/java/com/projects/thirtyseven/glue/CustomAddAuthorAdapter.java
@@ -0,0 +1,44 @@
+package com.projects.thirtyseven.glue;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by alexwalker on 10.06.17.
+ */
+
+class CustomAddAuthorAdapter extends ArrayAdapter {
+    public CustomAddAuthorAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List author) {
+        super(context, resource, author);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View gridViewItem = convertView;
+        if (gridViewItem == null) {
+            gridViewItem = LayoutInflater.from(getContext()).inflate(R.layout.custom_gridview_item, parent, false);
+        }
+
+        Author currentAuthor = getItem(position);
+
+        ImageView photo = (ImageView) gridViewItem.findViewById(R.id.authorPhotoImageView);
+        TextView name = (TextView) gridViewItem.findViewById(R.id.authorNameTextView);
+
+
+        name.setText(currentAuthor.getName);
+        photo.setImageDrawable(currentAuthor.getPhoto);
+
+        return gridViewItem;
+    }
+}
