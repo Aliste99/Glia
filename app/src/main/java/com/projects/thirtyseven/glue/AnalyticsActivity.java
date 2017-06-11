31382ab7c90001973913e8c6b5e95e1ed87a711a
Rinatius <rinat@kloop.kg>
2017-06-11 09:30:26 +0600

    Adapt getting Google Analytics data to Android

--- app/src/main/java/com/projects/thirtyseven/glue/AnalyticsActivity.java
+++ app/src/main/java/com/projects/thirtyseven/glue/AnalyticsActivity.java
@@ -1,29 +1,39 @@
 package com.projects.thirtyseven.glue;
 
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 import com.google.api.services.analyticsreporting.v4.AnalyticsReportingScopes;
 import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
@@ -46,18 +56,17 @@ public class AnalyticsActivity extends AppCompatActivity {
 
     // Path to client_secrets.json file downloaded from the Developer's Console.
     // The path is relative to HelloAnalytics.java.
-    private static final String CLIENT_SECRET_JSON_RESOURCE = "client_secrets.json";
+    //private static final String CLIENT_SECRET_JSON_RESOURCE = "client_id.json";
 
     // Replace with your view ID.
     private static final String VIEW_ID = "ga:26575673";
 
     // The directory where the user's credentials will be stored.
-    private static final File DATA_STORE_DIR = new File(
-            System.getProperty("user.home"), ".store/hello_analytics");
+    private static final File DATA_STORE_DIR = new File(Environment.getExternalStorageDirectory()+File.separator+"hello_analytics");;
 
     private static final String APPLICATION_NAME = "Hello Analytics Reporting";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static NetHttpTransport httpTransport;
+    private static HttpTransport httpTransport;
     private static FileDataStoreFactory dataStoreFactory;
 
     @Override
@@ -71,14 +80,14 @@ public class AnalyticsActivity extends AppCompatActivity {
         getAnalyticsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    AnalyticsReporting service = initializeAnalyticsReporting();
 
-                    GetReportsResponse response = getReport(service);
-                    printResponse(response);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                //File file = new File(CLIENT_SECRET_JSON_RESOURCE);
+                //if(file.exists()) gAnalyticsTextView.setText("File exists!");
+                //else gAnalyticsTextView.setText("File DOES NOT exist!");
+
+                new PrinAnalyticResponse().execute();
+
+
             }
         });
     }
@@ -92,23 +101,29 @@ public class AnalyticsActivity extends AppCompatActivity {
      */
     private static AnalyticsReporting initializeAnalyticsReporting() throws GeneralSecurityException, IOException {
 
-        httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        httpTransport = AndroidHttp.newCompatibleTransport();
         dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+
+
+        String secretString = "{\"installed\":{\"client_id\":\"210790084215-ap7n3pbvt0d0lp16viqne64228408v21.apps.googleusercontent.com\",\"project_id\":\"rapid-math-170220\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://accounts.google.com/o/oauth2/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http://localhost\"]}}";
+        InputStream stream = new ByteArrayInputStream(secretString.getBytes(StandardCharsets.UTF_8));
 
         // Load client secrets.
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-                new InputStreamReader(AnalyticsActivity.class
-                        .getResourceAsStream(CLIENT_SECRET_JSON_RESOURCE)));
+                new InputStreamReader(stream));
 
         // Set up authorization code flow for all authorization scopes.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
                 .Builder(httpTransport, JSON_FACTORY, clientSecrets,
                 AnalyticsReportingScopes.all()).setDataStoreFactory(dataStoreFactory)
                 .build();
-
+        String url = flow.newAuthorizationUrl().setRedirectUri("http://localhost").build();
+        Log.i("Glia", url);
         // Authorize.
-        Credential credential = new AuthorizationCodeInstalledApp(flow,
-                new LocalServerReceiver()).authorize("user");
+        LocalServerReceiver receiver = new LocalServerReceiver();
+        AuthorizationCodeInstalledApp authCodeInstalledApp = new AuthorizationCodeInstalledApp(flow,
+                receiver);
+        Credential credential = authCodeInstalledApp.authorize("user");
         // Construct the Analytics Reporting service object.
         return new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME).build();
@@ -173,7 +188,7 @@ public class AnalyticsActivity extends AppCompatActivity {
             List<ReportRow> rows = report.getData().getRows();
 
             if (rows == null) {
-                System.out.println("No data found for " + VIEW_ID);
+                Log.i("Glia", "No data found for " + VIEW_ID);
                 return;
             }
 
@@ -181,17 +196,41 @@ public class AnalyticsActivity extends AppCompatActivity {
                 List<String> dimensions = row.getDimensions();
                 List<DateRangeValues> metrics = row.getMetrics();
                 for (int i = 0; i < dimensionHeaders.size() && i < dimensions.size(); i++) {
-                    System.out.println(dimensionHeaders.get(i) + ": " + dimensions.get(i));
+                    Log.i("Glia", dimensionHeaders.get(i) + ": " + dimensions.get(i));
                 }
 
                 for (int j = 0; j < metrics.size(); j++) {
-                    System.out.print("Date Range (" + j + "): ");
+                    Log.i("Glia", "Date Range (" + j + "): ");
                     DateRangeValues values = metrics.get(j);
                     for (int k = 0; k < values.getValues().size() && k < metricHeaders.size(); k++) {
-                        System.out.println(metricHeaders.get(k).getName() + ": " + values.getValues().get(k));
+                        Log.i("Glia", metricHeaders.get(k).getName() + ": " + values.getValues().get(k));
                     }
                 }
             }
         }
     }
+
+
+    private class PrinAnalyticResponse extends AsyncTask<Integer, Integer, GetReportsResponse> {
+        protected GetReportsResponse doInBackground(Integer... num) {
+            GetReportsResponse response = null;
+            try {
+                AnalyticsReporting service = initializeAnalyticsReporting();
+
+                response = getReport(service);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected void onPostExecute(GetReportsResponse response) {
+            printResponse(response);
+        }
+
+        }
 }
