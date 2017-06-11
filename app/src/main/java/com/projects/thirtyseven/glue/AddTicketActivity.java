e8e47d9907ea96f1df037837729ef2bdd362d70c
NismoSkyline <nismoskyr34@gmail.com>
2017-06-11 17:16:02 +0600

    Colored tag mark in listView

--- app/src/main/java/com/projects/thirtyseven/glue/AddTicketActivity.java
+++ app/src/main/java/com/projects/thirtyseven/glue/AddTicketActivity.java
@@ -74,7 +74,7 @@ public class AddTicketActivity extends AppCompatActivity{
 
     private void setSpinnerAdapter() {
         final ArrayList<ItemData> list = new ArrayList<>();
-        list.add(new ItemData("Текст написан", R.drawable.red_tag));
+        list.add(new ItemData(getString(R.string.text_wrote), R.drawable.red_tag));
         list.add(new ItemData("Текст проверен", R.drawable.green_tag));
         list.add(new ItemData("Видео собрано", R.drawable.blue_tag));
         list.add(new ItemData("Видео смонтированно", R.drawable.orange_tag));
@@ -169,8 +169,6 @@ public class AddTicketActivity extends AppCompatActivity{
                 dialog.show();
 
             }
-
-
         });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -182,7 +180,7 @@ public class AddTicketActivity extends AppCompatActivity{
                 ticket.setTicketTag(ticketTag.getText().toString());
                 ticket.setTicketCategory(ticketCategory.getText().toString());
                 ticket.setTicketDescription(ticketDescription.getText().toString());
-                ticket.setTicketTaskProfession(ticketTaskProfession.getText().toString());
+                ticket.setTicketTaskProfession(ticketTaskProfession.getText     ().toString());
                 ticket.setTicketTaskCoWorker(ticketTaskCoWorker.getText().toString());
                 ticket.setTicketTaskFee(ticketTaskFee.getText().toString());
                 ticket.setTicketExpenses(ticketExpenses.getText().toString());
--- app/src/main/java/com/projects/thirtyseven/glue/CustomTicketAdapter.java
+++ app/src/main/java/com/projects/thirtyseven/glue/CustomTicketAdapter.java
@@ -1,18 +1,24 @@
 package com.projects.thirtyseven.glue;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
-public class CustomTicketAdapter extends ArrayAdapter<Ticket>{
+public class CustomTicketAdapter extends ArrayAdapter<Ticket> {
 
     private Context context;
 
@@ -31,14 +37,24 @@ public class CustomTicketAdapter extends ArrayAdapter<Ticket>{
         }
 
         Ticket ticket = getItem(position);
-
+        String getTagText = ticket.getTicketTag();
         TextView name = (TextView) listViewItem.findViewById(R.id.name);
-        TextView tag = (TextView) listViewItem.findViewById(R.id.tagText);
+        TextView category = (TextView) listViewItem.findViewById(R.id.tagText);
         TextView description = (TextView) listViewItem.findViewById(R.id.description);
-       // TextView views = (TextView) listViewItem.findViewById(R.id.views);
+        ImageView tagView = (ImageView) listViewItem.findViewById(R.id.tagMark);
+        // TextView views = (TextView) listViewItem.findViewById(R.id.views);
 
         name.setText(ticket.getTicketTitle());
-        tag.setText(ticket.getTicketTag());
+        category.setText(getTagText);
+        String tagText = context.getString(R.string.text_wrote);
+
+        tagView.setImageResource(R.drawable.bookmark);
+
+        if (tagText.equalsIgnoreCase(getTagText )){
+            tagView.setColorFilter(ContextCompat.getColor(context, R.color.blackColor));
+        }else
+            tagView.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimary));
+
         description.setText(ticket.getTicketDescription());
         //views.setText("12563");
 
